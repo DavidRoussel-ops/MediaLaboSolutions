e0e076c993b0d50e6bbe13d057b5c47be4fa51ee
David <davidroussel171113@outlook.fr>
2025-11-05 22:49:25 +0100

    Ajout du dossier pour le micro service back

---
+++ mediaLaboSolutionsMicroBack/src/test/java/com/example/mediaLaboSolutionsMicroBack/MediaLaboSolutionsMicroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediaLaboSolutionsMicroBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLaboSolutionsMicroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
