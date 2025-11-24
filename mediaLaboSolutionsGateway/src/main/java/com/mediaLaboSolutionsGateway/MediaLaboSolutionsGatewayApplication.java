4b4cf09e28d90239e94eb6d6a789da2a88ae1136
David <davidroussel171113@outlook.fr>
2025-11-24 21:40:39 +0100

    Ajoput d'une base de microservice pour la partie Gateway

---
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/MediaLaboSolutionsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mediaLaboSolutionsGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLaboSolutionsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaLaboSolutionsGatewayApplication.class, args);
+	}
+
+}
---
+++ mediaLaboSolutionsGateway/src/test/java/com/mediaLaboSolutionsGateway/MediaLaboSolutionsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaLaboSolutionsGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLaboSolutionsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
