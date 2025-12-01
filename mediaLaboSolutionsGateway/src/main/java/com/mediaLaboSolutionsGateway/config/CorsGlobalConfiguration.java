5070a30ee25c1ec8b63ffc4a763fbf59e83ad737
David <davidroussel171113@outlook.fr>
2025-12-01 23:38:26 +0100

    Ajout d'une class de configuration pour les requête CORS du front

--- mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/CorsGlobalConfiguration.java
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/CorsGlobalConfiguration.java
@@ -3,8 +3,9 @@ package com.mediaLaboSolutionsGateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ import java.util.Arrays;
 public class CorsGlobalConfiguration {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
@@ -22,6 +23,6 @@ public class CorsGlobalConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        return new CorsWebFilter(source);
     }
 }
