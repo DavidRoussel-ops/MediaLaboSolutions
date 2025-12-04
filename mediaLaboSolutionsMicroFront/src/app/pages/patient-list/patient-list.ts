45a3a51fe7224bbd11c521bd361005a0914bad47
David <davidroussel171113@outlook.fr>
2025-12-04 23:09:08 +0100

    Ajout du formulaire à la page de list patient

--- mediaLaboSolutionsMicroFront/src/app/pages/patient-list/patient-list.ts
+++ mediaLaboSolutionsMicroFront/src/app/pages/patient-list/patient-list.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PatientService } from '../../services/patient';
 import { HttpClientModule } from '@angular/common/http';
+import { PatientForm } from '../patient-form/patient-form';
 
 @Component({
   selector: 'app-patient-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, PatientForm],
   templateUrl: './patient-list.html',
   styleUrls: ['./patient-list.scss']
 })
 export class PatientList implements OnInit {
   patients = signal<any[]>([]);
   loaded = signal(false);
+  showForm = false;
 
   constructor(private patientService: PatientService) {}
 
@@ -23,5 +25,32 @@ export class PatientList implements OnInit {
       this.patients.set(data);
       this.loaded.set(true);
     });
+  }
+
+  /*addPatient(patient : any) {
+    console.log("Formulaire reçu ajout : ", patient);
+    this.patientService.addPatient(patient).subscribe(() => {
+      console.log("Patient bien enregistrer.");
+      this.loadPatients();
+      this.showForm = false;
+    })
+  }*/
+
+  addPatient(patient: any): void {
+  console.log("Formulaire reçu (ajout) :", patient);
+  this.patientService.addPatient(patient).subscribe(() => {
+    console.log("Patient ajouté !");
+    this.loadPatients();
+  });
+}
+
+  loadPatients() {
+    this.patientService.getAllPatients().subscribe(data => {
+      this.patients.set(data);
+    })
+  }
+
+  toogleForm() {
+    this.showForm = !this.showForm;
   }
 }
