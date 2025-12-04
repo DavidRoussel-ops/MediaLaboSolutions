4fae6ca1918d4bc0c5d4be1423689469ba984d56
David <davidroussel171113@outlook.fr>
2025-12-04 23:08:42 +0100

    Ajout du formulaire à la page de détails patient

--- mediaLaboSolutionsMicroFront/src/app/pages/patient-detail/patient-detail.ts
+++ mediaLaboSolutionsMicroFront/src/app/pages/patient-detail/patient-detail.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PatientService } from '../../services/patient';
+import { PatientForm } from '../patient-form/patient-form';
 
 @Component({
   selector: 'app-patient-detail',
   standalone : true,
-  imports: [],
+  imports: [PatientForm],
   templateUrl: './patient-detail.html',
   styleUrl: './patient-detail.scss',
 })
@@ -13,6 +14,7 @@ export class PatientDetail implements OnInit {
 
   patient = signal<any | null>(null);
   loaded = signal(false);
+  showForm = false;
 
   constructor(
     private route : ActivatedRoute,
@@ -28,4 +30,19 @@ export class PatientDetail implements OnInit {
     });
   }
 
+  updatePatient(patient : any) {
+    const id = this.route.snapshot.params['id'];
+    console.log("Formulaire reçu MAJ : ", patient);
+    this.patientService.updatePatient(id, patient).subscribe(() => {
+      console.log("Données du patient bien mise à jour.");
+      this.patientService.getPatientById(id).subscribe(data => {
+        this.patient.set(data);
+      });
+      this.showForm = false;
+    });
+  }
+
+  toogleForm() {
+    this.showForm = !this.showForm;
+  }
 }
