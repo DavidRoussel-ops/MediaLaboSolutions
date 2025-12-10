544af32be9e6337b40aefe3fe9693ee8243d9af5
David <davidroussel171113@outlook.fr>
2025-12-10 22:10:51 +0100

    Mise en place d'un nouveau microservice pour la prise en charge des notes avec spring initialzr

---
+++ mediaLaboSolutionsMicroMongoDB/src/test/java/com/mediaLaboSolutionsMicroMongoDB/MediaLaboSolutionsMicroMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaLaboSolutionsMicroMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLaboSolutionsMicroMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
