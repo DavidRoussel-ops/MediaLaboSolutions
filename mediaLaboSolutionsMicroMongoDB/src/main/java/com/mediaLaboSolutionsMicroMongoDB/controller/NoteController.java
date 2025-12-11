40e21549bd138a6e0bf5e6da6e4b28d5cb983b8d
David <davidroussel171113@outlook.fr>
2025-12-11 22:29:26 +0100

    Ajout d'une couche controller pour le model Note

---
+++ mediaLaboSolutionsMicroMongoDB/src/main/java/com/mediaLaboSolutionsMicroMongoDB/controller/NoteController.java
@@ -0,0 +1,56 @@
+package com.mediaLaboSolutionsMicroMongoDB.controller;
+
+import com.mediaLaboSolutionsMicroMongoDB.entity.Note;
+import com.mediaLaboSolutionsMicroMongoDB.service.NoteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    private final NoteService service;
+
+    public NoteController(NoteService noteService) {
+        this.service = noteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> addNote(@RequestBody Note note) {
+        Note saved = service.addNote(note);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping
+    public List<Note> getAllNotes() {return service.getAllNotes();}
+
+    @GetMapping("/patient/{patId}")
+    public List<Note> getNotesByPatient(@PathVariable Long patId) {
+        return service.getNotesByPatientId(patId);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+        return service.getNoteById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable Long id, @RequestBody Note note) {
+        return service.updateNote(id, note)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteNote(@PathVariable Long id) {
+        return service.deleteNote(id)
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
+}
