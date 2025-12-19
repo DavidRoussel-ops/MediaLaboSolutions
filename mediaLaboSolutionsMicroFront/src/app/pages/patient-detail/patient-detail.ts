c7855d7fba00c103cfdd18340043decc81d42d8d
David <davidroussel171113@outlook.fr>
2025-12-19 21:33:03 +0100

    Ajout des notes des patients et du formulaire pour en ajouter

--- mediaLaboSolutionsMicroFront/src/app/pages/patient-detail/patient-detail.ts
+++ mediaLaboSolutionsMicroFront/src/app/pages/patient-detail/patient-detail.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PatientService } from '../../services/patient';
 import { PatientForm } from '../patient-form/patient-form';
+import { Note, NoteService } from '../../services/note';
+import { NoteForm } from '../note-form/note-form';
 
 @Component({
   selector: 'app-patient-detail',
   standalone : true,
-  imports: [PatientForm],
+  imports: [PatientForm, NoteForm],
   templateUrl: './patient-detail.html',
   styleUrl: './patient-detail.scss',
 })
@@ -15,10 +17,15 @@ export class PatientDetail implements OnInit {
   patient = signal<any | null>(null);
   loaded = signal(false);
   showForm = false;
+  patientId! : number;
+  patientName : string = '';
+  notes = signal<Note[]>([]);
+  showFormNote = false;
 
   constructor(
     private route : ActivatedRoute,
-    private patientService : PatientService
+    private patientService : PatientService,
+    private noteService : NoteService
   ) {}
 
   ngOnInit(): void {
@@ -28,6 +35,9 @@ export class PatientDetail implements OnInit {
       this.patient.set(data);
       this.loaded.set(true);
     });
+    this.patientId = Number(this.route.snapshot.paramMap.get('id'));
+    this.patientService.getPatientById(this.patientId).subscribe(p => this.patientName = p.nom);
+    this.loadNotes();
   }
 
   updatePatient(patient : any) {
@@ -44,5 +54,27 @@ export class PatientDetail implements OnInit {
 
   toogleForm() {
     this.showForm = !this.showForm;
+  }
+
+  toogleFormNote() {
+    this.showFormNote = !this.showFormNote;
+  }
+
+  loadNotes() : void {
+    this.noteService.getNotesByPatient(this.patientId).subscribe(data => {
+      this.notes.set(data);
+    });
+  }
+
+  handleNoteAdded(noteText : string) : void {
+    const note : Note = {
+      patId : this.patientId,
+      patient : this.patientName,
+      note : noteText
+    };
+
+    this.noteService.addNote(note).subscribe(saved => {
+      this.notes.update(current => [...current, saved]);
+    });
   }
 }
