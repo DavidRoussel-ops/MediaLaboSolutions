197d1acf4fed44856e6a673ff1b529eb1271f904
David <davidroussel171113@outlook.fr>
2025-12-19 21:31:47 +0100

    Modification pour prise en charge de note

--- mediaLaboSolutionsMicroFront/src/app/services/patient.ts
+++ mediaLaboSolutionsMicroFront/src/app/services/patient.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class PatientService {
   private baseUrl = "/api/patients";
   private headers = new HttpHeaders({
-    "Authorization" : "Basic " + btoa("admin:admin1234!")
+    "Authorization" : "Basic " + btoa("admin:admin123")
   });
 
   constructor(private http : HttpClient) {}
