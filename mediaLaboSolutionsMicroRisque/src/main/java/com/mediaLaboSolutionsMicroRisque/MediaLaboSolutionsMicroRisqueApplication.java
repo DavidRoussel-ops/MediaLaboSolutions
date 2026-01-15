e512e2b82d3800f6514a559b2861325a269cdcc3
David <davidroussel171113@outlook.fr>
2026-01-15 22:06:33 +0100

    Ajout d'un nouveau micro service pour la prise en compte des risques de chaque patient

---
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/MediaLaboSolutionsMicroRisqueApplication.java
@@ -0,0 +1,13 @@
+package com.mediaLaboSolutionsMicroRisque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLaboSolutionsMicroRisqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaLaboSolutionsMicroRisqueApplication.class, args);
+	}
+
+}
---
+++ mediaLaboSolutionsMicroRisque/src/test/java/com/mediaLaboSolutionsMicroRisque/MediaLaboSolutionsMicroRisqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaLaboSolutionsMicroRisque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLaboSolutionsMicroRisqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
