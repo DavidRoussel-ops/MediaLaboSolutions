f9a82cd2dac39c35d47ad49005502a77792420be
David <davidroussel171113@outlook.fr>
2026-01-15 22:59:47 +0100

    Ajout d'une entité représentant le patient

---
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/entity/PatientDto.java
@@ -0,0 +1,50 @@
+package com.mediaLaboSolutionsMicroRisque.entity;
+
+public class PatientDto {
+
+    private Integer id;
+    private String family;
+    private String given;
+    private String gender;
+    private String dob;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getGiven() {
+        return given;
+    }
+
+    public void setGiven(String given) {
+        this.given = given;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
