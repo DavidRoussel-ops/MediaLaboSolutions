14d7e609053a5c7ed87ed673a3a0395cffec38e4
David <davidroussel171113@outlook.fr>
2026-01-15 23:02:34 +0100

    Ajout d'une entité représentant le résultat potentiel à afficher dans le message

---
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/entity/AssessmentResult.java
@@ -0,0 +1,68 @@
+package com.mediaLaboSolutionsMicroRisque.entity;
+
+public class AssessmentResult {
+
+    private Integer patientId;
+    private String patientName;
+    private int age;
+    private String gender;
+    private int triggerCount;
+    private RiskLevel riskLevel;
+    private String message;
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getTriggerCount() {
+        return triggerCount;
+    }
+
+    public void setTriggerCount(int triggerCount) {
+        this.triggerCount = triggerCount;
+    }
+
+    public RiskLevel getRiskLevel() {
+        return riskLevel;
+    }
+
+    public void setRiskLevel(RiskLevel riskLevel) {
+        this.riskLevel = riskLevel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
