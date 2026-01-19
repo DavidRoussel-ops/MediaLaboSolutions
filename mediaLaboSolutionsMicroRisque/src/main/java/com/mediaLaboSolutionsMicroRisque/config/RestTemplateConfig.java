10628a45635b0314c597b49a4899e931f3a36944
David <davidroussel171113@outlook.fr>
2026-01-19 22:14:54 +0100

    Mise en place d'un RestTemplate prenant en compte une authentification pour validation test Postman

--- mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/config/RestTemplateConfig.java
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/config/RestTemplateConfig.java
@@ -4,12 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            String auth = "admin:admin123";
+            byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
+            String authHeader = "Basic " + new String(encodedAuth);
+            request.getHeaders().add("Authorization", authHeader);
+            return execution.execute(request, body);
+        });
+        return restTemplate;
     }
 
 }
