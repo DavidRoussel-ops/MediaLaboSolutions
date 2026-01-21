6aac7957647f7cbb9f767546bc8a4695b72b9ca7
David <davidroussel171113@outlook.fr>
2026-01-21 22:14:33 +0100

    Retrait commentaire inutile et import associer

--- mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/SpringSecurityConfig.java
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/SpringSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.MapReactiveUserDetailsServi
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -23,7 +22,6 @@ public class SpringSecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .cors(corsSpec -> {})
                 .authorizeExchange(exchanges -> exchanges
-                        //.pathMatchers("/api/**").authenticated()
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().authenticated()
                 )
