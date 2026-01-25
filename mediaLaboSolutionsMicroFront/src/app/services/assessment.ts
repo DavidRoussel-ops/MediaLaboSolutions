757dfc2b756c5d9d5aee91c1d0449c1582163b2c
David <davidroussel171113@outlook.fr>
2026-01-25 20:14:16 +0100

    Ajout d'un service pour l'évaluation

---
+++ mediaLaboSolutionsMicroFront/src/app/services/assessment.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Assessment {
+  patientId : number;
+  patientName : string;
+  age : number;
+  gender : string;
+  triggerCount : number;
+  riskLevel : string;
+  message : string;
+}
+
+@Injectable({
+  providedIn : 'root'
+})
+export class AssessmentService {
+
+  private apiUrl = 'http://localhost:8080/assess';
+
+  constructor(private http : HttpClient) {}
+
+  getAssessment(patientId : number) : Observable<Assessment> {
+    const headers = new HttpHeaders({
+      Authorization : 'Basic ' + btoa("admin:admin123")
+    });
+    return this.http.get<Assessment>(`${this.apiUrl}/${patientId}`, { headers });
+  }
+}
