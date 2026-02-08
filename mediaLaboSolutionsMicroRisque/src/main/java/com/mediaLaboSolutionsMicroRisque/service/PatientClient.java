f13bbe4f24895b5567645fd80e0237dff8ed1ca1
David <davidroussel171113@outlook.fr>
2026-02-08 23:14:51 +0100

    Appel à l'URL utiliser par Docker

--- mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/service/PatientClient.java
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/service/PatientClient.java
@@ -15,7 +15,7 @@ public class PatientClient {
 
     public PatientDto getPatientById(Integer id) {
         return restTemplate.getForObject(
-                "http://localhost:8081/api/patients/" + id,
+                "http://patient-service:8081/api/patients/" + id,
                 PatientDto.class
         );
     }
