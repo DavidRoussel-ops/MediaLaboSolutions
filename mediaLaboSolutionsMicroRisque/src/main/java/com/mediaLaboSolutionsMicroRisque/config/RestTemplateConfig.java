bda0f706500a77ce5be2d37b55855b76aba42c63
David <davidroussel171113@outlook.fr>
2026-02-08 23:13:41 +0100

    Retrait de la mise ne place d'un user suite au retrait de la sécurité spring

--- mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/config/RestTemplateConfig.java
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/config/RestTemplateConfig.java
@@ -12,16 +12,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-            String auth = "admin:admin123";
-            byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
-            String authHeader = "Basic " + new String(encodedAuth);
-            request.getHeaders().add("Authorization", authHeader);
-            return execution.execute(request, body);
-        });
-        return restTemplate;
+        return new RestTemplate();
     }
 
 }
