982cdf9ae8836a1dc27f05e85be2638934468172
David <davidroussel171113@outlook.fr>
2026-02-08 23:14:42 +0100

    Appel à l'URL utiliser par Docker

--- mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/service/NotesClient.java
+++ mediaLaboSolutionsMicroRisque/src/main/java/com/mediaLaboSolutionsMicroRisque/service/NotesClient.java
@@ -19,7 +19,7 @@ public class NotesClient {
     
     public List<NoteDto> getNotesByPatient(Integer id) {
         return restTemplate.exchange(
-                "http://localhost:8082/api/notes/patient/" + id,
+                "http://notes-service:8082/api/notes/patient/" + id,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<NoteDto>>() {}
