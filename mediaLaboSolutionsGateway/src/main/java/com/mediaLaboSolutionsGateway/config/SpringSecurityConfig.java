53df141c589835e6d14b33df672021f8c66b1c02
David <davidroussel171113@outlook.fr>
2026-02-14 21:56:11 +0100

    Ajout concernant le healthcheck du Gateway

--- mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/SpringSecurityConfig.java
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/SpringSecurityConfig.java
@@ -22,6 +22,7 @@ public class SpringSecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .cors(corsSpec -> {})
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/health").permitAll()
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().authenticated()
                 )
