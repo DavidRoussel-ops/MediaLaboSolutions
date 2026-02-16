4eea882e63943abe47279844c0d16936a7ba5851
David <davidroussel171113@outlook.fr>
2026-02-16 20:53:10 +0100

    Correction faute de frappe

--- mediaLaboSolutionsMicroFront/src/app/services/note.ts
+++ mediaLaboSolutionsMicroFront/src/app/services/note.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { API_URL } fron '../api.config';
+import { API_URL } from '../api.config';
 
 export interface Note {
     id? : string;
