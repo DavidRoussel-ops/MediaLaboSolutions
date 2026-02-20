c1eae6fa3d550041d02769c363a189c269e4eca2
David <davidroussel171113@outlook.fr>
2026-02-20 13:28:11 +0100

    Modification de l'origin de l'appel pour test de mise en place d'un system reposant uniquement sur docker

--- mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/CorsGlobalConfiguration.java
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/config/CorsGlobalConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import java.util.Arrays;
@@ -16,7 +15,7 @@ public class CorsGlobalConfiguration {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://192.168.1.11:4200"));
+        config.addAllowedOriginPattern("*");
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
