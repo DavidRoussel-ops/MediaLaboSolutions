f0483b34fc28f5523aad8d1a64c89896d0befd34
David <davidroussel171113@outlook.fr>
2026-03-03 21:37:28 +0100

    Clean code

--- mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/MediaLaboSolutionsGatewayApplication.java
+++ mediaLaboSolutionsGateway/src/main/java/com/mediaLaboSolutionsGateway/MediaLaboSolutionsGatewayApplication.java
@@ -3,9 +3,16 @@ package com.mediaLaboSolutionsGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Point d'entrée principal de l'application
+ */
 @SpringBootApplication
 public class MediaLaboSolutionsGatewayApplication {
 
+	/**
+	 * Méthode principale de lancement
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MediaLaboSolutionsGatewayApplication.class, args);
 	}
