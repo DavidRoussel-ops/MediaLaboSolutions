610093b42becbac8d13800a261590b5c1751db28
David <davidroussel171113@outlook.fr>
2026-03-04 22:30:51 +0100

    Ajout commentaire

--- mediaLaboSolutionsMicroMongoDB/src/main/java/com/mediaLaboSolutionsMicroMongoDB/MediaLaboSolutionsMicroMongoDbApplication.java
+++ mediaLaboSolutionsMicroMongoDB/src/main/java/com/mediaLaboSolutionsMicroMongoDB/MediaLaboSolutionsMicroMongoDbApplication.java
@@ -3,9 +3,16 @@ package com.mediaLaboSolutionsMicroMongoDB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Point d'entrée principal de l'application
+ */
 @SpringBootApplication
 public class MediaLaboSolutionsMicroMongoDbApplication {
 
+	/**
+	 * Méthode principale de lancement
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MediaLaboSolutionsMicroMongoDbApplication.class, args);
 	}
